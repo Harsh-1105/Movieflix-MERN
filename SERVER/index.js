e2abc05fc348f1d27e54543976434bb71511704f
Harsh <harsh1879.be20@chitkara.edu.in>
2024-06-28 15:43:22 +0530

    Changed Index.js

--- SERVER/index.js
+++ SERVER/index.js
@@ -8,7 +8,13 @@ import routes from "./src/routes/index.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors(
+  // {
+  //   origin:["https://deploy-mern-1whq.vercel.app"],
+  //   methods:["POST","GET"],
+  //   credentials:true
+  // }
+));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
